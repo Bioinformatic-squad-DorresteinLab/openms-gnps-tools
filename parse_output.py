2399540bb07d7064aff76250479a147859b0f18e
Abi Sarvepalli <abisarvepalli@gmail.com>
2019-02-21 03:58:39 -0800

    fix: copyfile

--- parse_output.py
+++ parse_output.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     output_dir = sys.argv[OUTPUT_DIR_INDEX]
     for input_dir in sys.argv[1:OUTPUT_DIR_INDEX]:
         print("move -- " + input_dir + " -> " + output_dir+"/"+input_dir)
-        shutil.copyfile(input_dir, output_dir+"/"+input_dir)
+        shutil.copytree(input_dir, output_dir+"/"+input_dir)
 
     # copy log files to log folder
     os.mkdir('logs')
@@ -39,4 +39,4 @@ if __name__ == '__main__':
         for filepath in os.listdir(input_dir):
             input_file = input_dir+'/'+filepath
             if 'log' in filepath:
-                shutil.copytree(input_file, output_dir+"/"+input_file)
+                shutil.copyfile(input_file, output_dir+"/"+input_file)
